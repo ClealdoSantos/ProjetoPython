0ab43c10d8ba46c5d0dc1b23cd55858d82d94c9c
Clealdo Santos <158526833+ClealdoSantos@users.noreply.github.com>
2024-03-19 19:51:22 -0300

    Update triangulos.py

--- triangulos.py
+++ triangulos.py
@@ -1,7 +1,7 @@
-print('\033[0;30;44m-=\033[m'*20)
+print('\033[7;30;44m-=\033[m'*20)
 print('ANALISANDOR DE TRIÂNGULOS')
 print('\033[0;30;44m-=\033[m'*20)
-r1 = float(input('Primeiro segmento:'))
+r1 = float(input('Primeiro segmento: '))
 r2 = float(input('Segundo segmento: '))
 r3 = float(input('terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
