e6d0f1f6db7d84e57e558b7e412ee2e7d715ce19
Clealdo Santos <158526833+ClealdoSantos@users.noreply.github.com>
2024-03-19 19:34:19 -0300

    Update triangulos.py

--- triangulos.py
+++ triangulos.py
@@ -1,10 +1,10 @@
-print('-='*20)
+print('\033[0;33;44m-=\033[m'*20)
 print('ANALISANDOR DE TRIÂNGULOS')
-print('-='*20)
+print('\033[0;33;44m-=\033[m'*20)
 r1 = float(input('Primeiro segmento:'))
 r2 = float(input('Segundo segmento: '))
 r3 = float(input('terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print('Os segmentos acima PODEM FORMAR triângulo!')
+    print('\033[0;30;44mOs segmentos acima PODEM FORMAR triângulo!')
 else:
-    print('Os segmentos acima NÃO FORMAM triângulo!')
+    print('\033[0;33;44Os segmentos acima NÃO FORMAM triângulo!')
