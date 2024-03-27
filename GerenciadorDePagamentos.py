027c2d00b6877fac4bd02af2e00cc937bcf292e1
Clealdo Santos <158526833+ClealdoSantos@users.noreply.github.com>
2024-03-27 17:25:20 -0300

    Update GerenciadorDePagamentos.py

--- GerenciadorDePagamentos.py
+++ GerenciadorDePagamentos.py
@@ -13,13 +13,13 @@ elif opção == 2:
 elif opção == 3:
     total = preço
     parcela = total / 2
-    print('Sua compra será parcelada em 2x de R${:.2f}'.format(parcela))
+    print('Sua compra será parcelada em 2x de R${:.2f} SEM JUROS'.format(parcela))
 elif opção == 4:
     total = preço + (preço * 20 / 100)
     totparc = int(input('Quantas parcelas? '))
     parcela = total / totparc
-    print('Sua compra será parcelada em {}x de R$  COM JUROS'.format(totparc, parcela))
+    print('Sua compra será parcelada em {}x de R${} COM JUROS'.format(totparc, parcela))
 else:
     total = preço
-    print('OPÇÃO IVÁLIDA de pagamento. Tente novamente!')
+    print('OPÇÃO INVÁLIDA de pagamento. Tente novamente!')
 print('Sua compra de R${:.2f} vai custar R${:.2f} no final.'.format(preço, total))
