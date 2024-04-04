947d27773c8d52675e45ef5e4a703254a5e00cb6
Clealdo Santos <158526833+ClealdoSantos@users.noreply.github.com>
2024-04-03 21:18:53 -0300

    Exercício para analisar idades entre sexos de pessoas.

---
+++ analisadordeIdade.py
@@ -0,0 +1,23 @@
+somaidade = 0
+médiaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
+for p in range(1, 5):
+    print('***** {}ª PESSOA *****'.format(p))
+    nome = str(input('NOME: ')).strip()
+    idade = int(input('IDADE: '))
+    sexo = str(input('SEXO [M/F]: ')).strip()
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+            totmulher20 += 1
+médiaidade = somaidade / 4
+print('A média de idade do grupo é de {} anos'.format(médiaidade))
+print('O homem mais velho tem \033[36m{}\033[m anos e se chama \033[036m{}\033[m'.format(maioridadehomem,nomevelho))
+print('Ao todo são \033[36m{}\033[m mulher(s) com menos de 20 anos'.format(totmulher20))
