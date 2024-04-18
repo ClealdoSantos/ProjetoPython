4329e9c0490990fc8ab63fee4c08c4c3cb895cf1
ClealdoSantos <158526833+ClealdoSantos@users.noreply.github.com>
2024-04-17 23:06:42 -0300

    Update somavalores.py

--- somavalores.py
+++ somavalores.py
@@ -3,5 +3,5 @@ núm = int(input('Digite um número [999 para parar]: '))
 while núm != 999:
      soma += núm
      cont += 1
-     núm = int(input('Digite um número[999 para parar]: '))
+     núm = int(input('Digite um número [999 para parar]: '))
 print('Você digitou {} e a soma foi {}'.format(cont, soma))
